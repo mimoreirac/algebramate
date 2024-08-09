21fefc63113267804ba7976fd722d46d59101048
mimoreirac <172557112+mimoreirac@users.noreply.github.com>
2024-08-08 19:50:00 -0500

    Add basic matrix functions

---
+++ funciones.py
@@ -0,0 +1,55 @@
+def LLenarMatriz(nombre):
+    print("Ingrese los datos de la matriz", nombre)
+    filas = int(input("Ingrese el número de filas: "))
+    columnas = int(input("Ingrese el número de columnas: "))
+    matriz = [[0 for _ in range(columnas)] for _ in range(filas)]
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            matriz[i][j] = int(input(f"Ingrese el número en la posición {i} , {j}: "))
+        
+    return matriz
+
+def ImprimirMatriz(matriz):
+    for i in matriz:
+        for j in i:
+            print(j, end=" ")
+        print()
+
+def CuadradaONo(matriz):
+    if(len(matriz) != len(matriz[0])):
+        matrizcuadrada = False
+    else:
+        matrizcuadrada = True
+
+    return matrizcuadrada
+
+def DeterminanteMatriz(matriz): # Utilizando el metodo de Laplace
+    if CuadradaONo(matriz):
+        if len(matriz) == 2:
+            determinante = matriz[0][0] * matriz[1][1] - matriz[1][0] * matriz[0][1]
+            return determinante
+        # Recursivo, si la matriz es mayor a 2x2
+        determinante = 0
+        for j in range(len(matriz)):
+            # Crea submatriz
+            submatriz = [[0 for _ in range(len(matriz)-1)] for _ in range(len(matriz)-1)]
+            for i in range(1, len(matriz)):
+                col = 0
+                for k in range(len(matriz)):
+                    if k != j:
+                        submatriz[i-1][col] = matriz[i][k]
+                        col += 1
+            
+            # Calcula cofactor y lo anade al determinante
+            cofactor = matriz[0][j] * DeterminanteMatriz(submatriz) * (-1) ** j
+            determinante += cofactor
+        
+        return determinante
+
+
+matrizA = LLenarMatriz("A")
+determinanteA = DeterminanteMatriz(matrizA)
+
+ImprimirMatriz(matrizA)
+print()
+print(f"El determinante es {determinanteA}")
